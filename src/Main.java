ae225ab3ed68dc216057b4758a6f71b9a9d76a45
Rayand <nopechan2020@gmail.com>
2022-04-14 18:39:55 +0300

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+
+public class Main {
+    public static void main(String[] args) {
+        int daysHavePassed = 234;
+        int dayOnWeek = 7;
+        int result;
+
+        System.out.println(" Программа позволяет расчитатать количество прошедших недель ");
+
+        result = daysHavePassed = 234 / dayOnWeek;
+
+        System.out.print(" Количество прошедших недель: " + result + " недели ");
+
+    }
+}
